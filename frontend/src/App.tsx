c3f097766db2581ae8b180b827d1b106c745c274
Alec Di Vito <divito.alec@gmail.com>
2021-01-16 22:38:55 +0200

    Got music streaming working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactHlsPlayer from 'react-hls-player';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactHlsPlayer
+        url='http://localhost:3000/stream/music_angela_paulson_dancing_waters_007.m3u8'
+        autoplay={false}
+        controls={true}
+        width="100%"
+        height="auto"
+      />
     </div>
   );
 }
