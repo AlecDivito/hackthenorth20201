913460236fcc0e9b627eca5bead1190bccfa3a27
Cindy Gu <cindygu@umich.edu>
2021-01-16 16:30:11 -0500

    added page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AddUserForm } from './components/AddUserForm';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <AddUserForm />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <AddUserForm />
+        </Route>
+        <Route path='/home'>
+          <div>
+            Home
+          </div>
+        </Route>
+        <Route path='/browse'>
+          <div>
+            Browse Soundscapes
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
