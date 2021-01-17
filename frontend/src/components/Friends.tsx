e756715558382a9c8949efd5156aea77f57fccf3
Cindy Gu <cindygu@umich.edu>
2021-01-17 01:06:15 -0500

    Started on Friends component

---
+++ frontend/src/components/Friends.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const people = [
+    { name: 'Marie Aldo', listening: 'What a Wonderful Life — Sally Soundtrack'},
+    { name: 'Kevin Vera', listening: 'Digital Versicolor — Glass Candy'},
+    { name: 'Chris Chu', listening: 'Calm Down'},
+    { name: 'Xavier Jzedjeck', listening: 'Heavy Rain'},
+    { name: 'Sarah Fumi', listening: 'Good Night' }
+];
+
+export const Friends: React.FC = () => {
+    return (
+        <div></div>
+    );
+};
